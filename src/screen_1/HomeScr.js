e0be86413574679088caec1fa68b5ce8f5c8634b
YuraChernii <67958005+YuraChernii@users.noreply.github.com>
2021-01-20 13:35:45 +0200

    Update HomeScr.js

--- src/screen_1/HomeScr.js
+++ src/screen_1/HomeScr.js
@@ -9,7 +9,7 @@ import {STARGATE_DETAILS} from "../route";
 
 
 
-const url = 'https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0' // на github проблемы c запросом, поэтому ссылку поменял
+const url = 'https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0'
 
 
 
